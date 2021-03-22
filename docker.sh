530331a01f9a0e06633de3c74b168554d68143ab
maximums <cdodi907@gmail.com>
2021-03-22 14:14:35 +0200

    project run

---
+++ docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#run docker container
+docker run -p 8000:4000 alexburlacu/rtp-server:faf18x
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# compile project
+rebar3 compile
+
+# run project
+rebar3 shell
